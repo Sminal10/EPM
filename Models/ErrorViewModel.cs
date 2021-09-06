2bdf5355afccc93e73f2520c20270a77ed41ee58
adityapatel9619 <adityapatel9619@gmail.com>
2021-09-06 14:03:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
