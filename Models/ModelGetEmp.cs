dd6a500a52000b78c208612fc04b470c1cdd4ea0
sminalkp10 <sminalkp10@gmail.com>
2021-09-30 00:46:39 +0530

    model get employee

---
+++ Models/ModelGetEmp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPM.Models
+{
+    public class ModelGetEmp
+    {
+        public string Empid { get; set; }
+        public string Fname { get; set; }
+        public string Mname { get; set; }
+        public string Lname { get; set; }
+        public string Bdate { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Mobilenum { get; set; }
+        public string Altnum { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Pincode { get; set; }
+        public string Salary { get; set; }
+        public string Startdate { get; set; }
+        public string Qualification { get; set; }
+    }
+}
