7b3daeb960526d695bdec69a6a0b091062cc86de
adityapatel9619 <adityapatel9619@gmail.com>
2021-09-30 00:50:39 +0530

    DB Get Employee

---
+++ DB/GetEmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPM.Models;
+
+namespace EPM.DB
+{
+    public class GetEmployee
+    {
+    }
+}
