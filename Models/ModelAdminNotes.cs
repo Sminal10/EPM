3b3197b02acd2b3cf3d85b489b205db0508afa58
adityapatel9619 <adityapatel9619@gmail.com>
2021-11-22 18:42:34 +0530

    Code Optimized

---
+++ Models/ModelAdminNotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPM.Models
+{
+    public class ModelAdminNotes
+    {
+        public string Id { get; set; }
+        public string Notes { get; set; }
+        public string AdminUsername { get; set; }
+    }
+}
