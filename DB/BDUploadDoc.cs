4c0141ca7a6fb2a9ca22a36229dec0dca339ef0d
adityapatel9619 <adityapatel9619@gmail.com>
2021-12-18 21:01:29 +0530

    Upload Document

---
+++ DB/BDUploadDoc.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPM.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace EPM.DB
+{
+    public class BDUploadDoc
+    {
+        public IEnumerable<ModelUploadDoc> GetUploadedDoc(string ConnectionString)
+        {
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                List<ModelUploadDoc> docs = new List<ModelUploadDoc>();
+                SqlCommand command = new SqlCommand("uspUploadDocSD", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@Id", SqlDbType.Int).Value = 0;
+                command.Parameters.Add("@Type", SqlDbType.Char).Value = "S";
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ModelUploadDoc doc = new ModelUploadDoc();
+                    doc.Id = reader["ID"].ToString();
+                    doc.Imagepath = reader["ImagePath"].ToString();
+                    doc.ADC = reader["AdhaarCardNum"].ToString();
+                    doc.PAN = reader["PanCardNum"].ToString();
+                    doc.BACC = reader["BnkAccNo"].ToString();
+                    doc.BIFC = reader["BnkIFSC"].ToString();
+                    doc.BBR = reader["BnkBranch"].ToString();
+                    doc.Empid = reader["EmpID"].ToString();
+                    docs.Add(doc);
+                }
+                con.Close();
+                return docs.ToArray();
+            } 
+        }
+
+        //POST
+        public string UploadDoc(ModelUploadDoc doc, string ConnectionString)
+        {
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                /*ImgLib.ImageLibrary library = new ImgLib.ImageLibrary();
+                var filename = "";
+                var base64String = doc.Imagepath.ToString();
+                filename = Guid.NewGuid().ToString();
+                //Saving
+                var Imgpath = library.SaveBase64StringInFolder(base64String, filename, "Image");*/
+
+                con.Open();
+
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand command = new SqlCommand("uspUploadDocAE", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ID", 0);
+                command.Parameters.AddWithValue("@Imagepath", doc.Imagepath);
+                command.Parameters.AddWithValue("@Adhaarcardnum", doc.ADC.ToString());
+                command.Parameters.AddWithValue("@Pancardnum", doc.PAN.ToString());
+                command.Parameters.AddWithValue("@BnkAccno", doc.BACC.ToString());
+                command.Parameters.AddWithValue("@BnkIFSC", doc.BIFC.ToString());
+                command.Parameters.AddWithValue("@BnkBranch", doc.BBR.ToString());
+                command.Parameters.AddWithValue("@Empid", doc.Empid.ToString());
+                command.Parameters.AddWithValue("@Type","UP");
+                command.ExecuteNonQuery();
+                string result = "Updated";
+                con.Close();
+                return result;
+            }
+        }
+    }
+}
---
+++ ImgLib/ImageLibrary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EPM.Models;
+
+namespace EPM.ImgLib
+{
+    public class ImageLibrary
+    {
+        public string[] SaveBase64StringInFolder(string base64String, string filename, string folderName)
+        {
+            var vfilename = "";
+            var filepath = "";
+            try
+            {
+                if(base64String.Length > (filename.Length + folderName.Length + 8))
+                {
+                    int extensionStartIndex = base64String.IndexOf('/');
+                    int extensionEndIndex = base64String.IndexOf(';');
+                    int filetypeStartIndex = base64String.IndexOf(':');
+                    extensionEndIndex = extensionEndIndex - extensionStartIndex;
+                    string fileExtension = base64String.Substring(extensionStartIndex + 1, extensionEndIndex - 1);
+                    string fileType = base64String.Substring(filetypeStartIndex + 1, extensionStartIndex);
+                    if (filename.Length > 50 || filename.Length < 1) 
+                    {
+                        vfilename = Guid.NewGuid().ToString();
+                    }
+                    else
+                    {
+                        vfilename = filename;
+                    }
+
+                    filename = vfilename + "." + fileExtension;
+                    base64String = base64String.Substring(base64String.IndexOf(",") + 1);
+                    if(base64String.Length > 0 && base64String != "undefined")
+                    {
+                        byte[] bytes = Convert.FromBase64String(base64String);
+                        var path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "wwwroot/" + folderName, filename);
+
+                        using (var imageFile = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                        {
+                            imageFile.Write(bytes, 0, bytes.Length);
+                            imageFile.Flush();
+                        };
+                    }
+                    filepath = "\\" + folderName + "\\" + filename;
+                }
+                else
+                {
+                    filepath = base64String;
+                    vfilename = filename;
+                }
+
+                return new[] { filepath, vfilename };
+            }
+            catch(Exception ex)
+            {
+                return new[] { "Error", ex.Message }; 
+            }
+        }
+    }
+}
