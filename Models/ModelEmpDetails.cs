ceeacb9cea35590133473221e7ccfbaa6e822f5b
adityapatel9619 <adityapatel9619@gmail.com>
2021-12-19 22:16:49 +0530

    Employee

---
+++ Models/ModelEmpDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPM.Models
+{
+    public class ModelEmpDetails
+    {
+        public string FName { get; set; }
+        public string MName { get; set; }
+        public string LName { get; set; }
+        public string Mnum { get; set; }
+        public string Altnum { get; set; }
+        public string Add { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Pincode { get; set; }
+    }
+}
