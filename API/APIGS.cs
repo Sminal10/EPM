5a919c0da1ca1ef4fa52eb582360475d84b03f17
adityapatel9619 <adityapatel9619@gmail.com>
2021-12-20 15:34:49 +0530

    emp

---
+++ API/APIGS.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPM.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EPM.API
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class APIGS : ControllerBase
+    {
+        DB.DBGenerateSalary data = new DB.DBGenerateSalary();
+        private readonly IOptions<ModelConnectionString> appsetting;
+
+        public APIGS(IOptions<ModelConnectionString> option)
+        {
+            appsetting = option;
+        }
+
+        // GET: api/<APIGS>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/APIGS/GetSInf?username=EPMAD01
+        [HttpGet]
+        [ActionName("GetSInf")]
+        public IEnumerable<ModelGS> GetGetInf(string username)
+        {
+            return data.GetGetInf(username, appsetting.Value.DefaultConnection);
+        }
+
+        // POST api/<APIGS>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<APIGS>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<APIGS>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DB/DBGenerateSalary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPM.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EPM.DB
+{
+    public class DBGenerateSalary
+    {
+        public IEnumerable<ModelGS> GetGetInf(string username, string ConnectionString)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                List<ModelGS> adminDetails = new List<ModelGS>();
+                SqlCommand comd = new SqlCommand("uspGenerateSalary", con);
+                comd.CommandType = CommandType.StoredProcedure;
+                comd.Parameters.AddWithValue("@usrname", username);
+                comd.Parameters.AddWithValue("@Type", "GS");
+                SqlDataReader reader = comd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ModelGS detail = new ModelGS();
+                    detail.Empid = reader["EmpId"].ToString();
+                    detail.Fname = reader["FName"].ToString();
+                    detail.Mname = reader["MName"].ToString();
+                    detail.Lname = reader["LName"].ToString();
+                    detail.Adcnum = reader["AdhaarCardNum"].ToString();
+                    detail.Pannum = reader["PanCardNum"].ToString();
+                    detail.BnkAcc = reader["BnkAccNo"].ToString();
+                    detail.BnkBr = reader["BnkBranch"].ToString();
+                    detail.BnkIfsc = reader["BnkIFSC"].ToString();
+                    detail.salary = reader["Salary"].ToString();
+
+                    adminDetails.Add(detail);
+                }
+                con.Close();
+                return adminDetails.ToArray();
+            }
+        }
+    }
+}
